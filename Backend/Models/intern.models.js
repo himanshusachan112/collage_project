9dcd4e59513f5e33295eac3dba64981af92e6271
RAHUL27474 <rahulchaudhary27474@gmail.com>
2024-08-18 15:41:03 +0530

    abcd

--- Backend/Models/intern.models.js
+++ Backend/Models/intern.models.js
@@ -1,29 +1,29 @@
-import mongoose, {Schema} from 'mongoose'
+// import mongoose, {Schema} from 'mongoose'
 
-const internSchema = new Schema({
-    role:{
-        type:String,
-        required:true,
-    },
-    company:{
-        type:String,
-        required:true,
-    },
-    description:{
-        type:String,
-        required:true,
-    },
-    stipend:{
-        type:String,
-    },
-    tenure:{
-        type:Number,
-    },
-    eligibility:{
-        type:Number,
-        required:true, 
-    }
+// const internSchema = new Schema({
+//     role:{
+//         type:String,
+//         required:true,
+//     },
+//     company:{
+//         type:String,
+//         required:true,
+//     },
+//     description:{
+//         type:String,
+//         required:true,
+//     },
+//     stipend:{
+//         type:String,
+//     },
+//     tenure:{
+//         type:Number,
+//     },
+//     eligibility:{
+//         type:Number,
+//         required:true, 
+//     }
 
-})
+// })
 
-export const Intern = mongoose.model("Intern",internSchema);
+// export const Intern = mongoose.model("Intern",internSchema);
--- Backend/Models/jobs.models.js
+++ Backend/Models/jobs.models.js
@@ -1,29 +1,29 @@
-import mongoose, {Schema} from 'mongoose'
+// import mongoose, {Schema} from 'mongoose'
 
-const jobSchema = new Schema({
-    role:{
-        type:String,
-        required:true,
-    },
-    company:{
-        type:String,
-        required:true,
-    },
-    description:{
-        type:String,
-        required:true,
-    },
-    stipend:{
-        type:String,
-    },
-    tenure:{
-        type:Number,
-    },
-    eligibility:{
-        type:Number,
-        required:true, 
-    }
+// const jobSchema = new Schema({
+//     role:{
+//         type:String,
+//         required:true,
+//     },
+//     company:{
+//         type:String,
+//         required:true,
+//     },
+//     description:{
+//         type:String,
+//         required:true,
+//     },
+//     stipend:{
+//         type:String,
+//     },
+//     tenure:{
+//         type:Number,
+//     },
+//     eligibility:{
+//         type:Number,
+//         required:true, 
+//     }
 
-})
+// })
 
-export const Intern = mongoose.model("Intern",jobSchema);
+// export const Intern = mongoose.model("Intern",jobSchema);
