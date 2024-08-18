dbc7e35245d52e70453b5700872ca386aab17fbb
RAHUL27474 <rahulchaudhary27474@gmail.com>
2024-08-19 00:16:25 +0530

    backend completed

--- Backend/routes/hackathon.js
+++ Backend/routes/hackathon.js
@@ -1,1 +1,15 @@
-// 
+const express=require("express");
+const router=express.Router();
+
+import { createHackathon , deleteHackathon, getHackathon} from '../controllers/hackathon.js'
+
+
+
+
+router.post("/createHackathon",createHackathon);
+router.post("/deleteHackathon",deleteHackathon);
+router.get("/getHackathon",getHackathon);
+
+
+
+module.exports = router;
--- Backend/routes/jobs.js
+++ Backend/routes/jobs.js
@@ -1,1 +1,15 @@
-// 
+const express=require("express");
+const router=express.Router();
+
+import { createJob , deleteJob, getJob} from '../controllers/jobs.js'
+
+
+
+
+router.post("/createJob",createJob);
+router.post("/deleteJob",deleteJob);
+router.get("/getJob",getJob);
+
+
+
+module.exports = router;
